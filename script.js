c756e345175d24206ea44b6f99f6452c396e6c1a
jackrichter <jackrichter1@gmail.com>
2024-09-15 04:28:21 +0200

    Intro to Geolocation Api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // prettier-ignore
 const months = [
@@ -16,10 +16,25 @@ const months = [
 	"December"
 ];
 
-const form = document.querySelector(".form");
-const containerWorkouts = document.querySelector(".workouts");
-const inputType = document.querySelector(".form__input--type");
-const inputDistance = document.querySelector(".form__input--distance");
-const inputDuration = document.querySelector(".form__input--duration");
-const inputCadence = document.querySelector(".form__input--cadence");
-const inputElevation = document.querySelector(".form__input--elevation");
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using the browser's Geolocation Api
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // Using 'Destructuring' to get the corresponding variables values from object coords.
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.se/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position.');
+    }
+  );
+}
